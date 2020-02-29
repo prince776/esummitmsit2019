073e4ac1f616f77e4951c61428ef1771ead54243
Dipanshu <52099879+dipanshubhola1009@users.noreply.github.com>
2020-02-29 20:42:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -416,7 +416,7 @@
         -------------------------------------*/
     var eventCounter = $("#countdown");
     if (eventCounter.length) {
-      eventCounter.countdown("2019/03/29", function(e) {
+      eventCounter.countdown("2020/03/16", function(e) {
         $(this).html(
           e.strftime(
             "<div class='countdown-section'><h2>%D</h2> <h3>day%!D</h3> </div><div class='countdown-section'><h2>%H</h2> <h3>Hour%!H</h3> </div><div class='countdown-section'><h2>%M</h2> <h3>Minutes</h3> </div><div class='countdown-section'><h2>%S</h2> <h3>Second</h3> </div>"
